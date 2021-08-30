e8738a3845d6f81d0040aa1e20cbae5545c4f520
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-08-29 22:58:53 -0700

    Working on Query.

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
