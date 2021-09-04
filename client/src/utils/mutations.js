9c7b0079bf9f382346be3830f4c9aefe76658c6b
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-09-04 13:37:31 -0700

    Updated case on inputBook (to InputBook)

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -49,7 +49,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($newBook: inputBook!) {
+  mutation saveBook($newBook: InputBook!) {
     saveBook(newBook: $newBook) {
       _id
       username
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,7 +23,7 @@ const typeDefs = gql`
     link: String
   }
 
-  input inputBook {
+  input InputBook {
     bookId: String
     authors: [String]
     title: String
@@ -39,7 +39,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(newBook: inputBook!): User
+    saveBook(newBook: InputBook!): User
     removeBook(bookId: ID!): User
   }
 `;
